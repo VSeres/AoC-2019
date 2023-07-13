c1b05496ddebdaeeb0f70a056b8d0b375a4ca0ce
Viktor Seres <viktor.seres@ibc.lan>
2023-07-13 09:23:19 +0200

    refactor day 3

--- day3/main.go
+++ day3/main.go
@@ -9,20 +9,27 @@ import (
 	"strings"
 )
 
-type point [2]int
-
 func main() {
-	wires := parseFile("./input.txt")
-	partOne(wires)
-	partTwo(wires)
+	wires := parseFile("./example.txt")
+	solve(wires)
 }
 
-func partOne(wires [][]string) {
-	pointMap := make(map[point]uint16)
+type point struct {
+	x int
+	y int
+}
 
+type info struct {
+	visited uint8
+	steps   uint
+}
+
+func solve(wires [][]string) {
+	pointMap := make(map[point]info)
 	for _, wire := range wires {
-		visited := make(map[point]bool)
+		visited := make(map[point]*info)
 		x, y := 0, 0
+		var steps uint = 0
 		for _, inst := range wire {
 			num, err := strconv.Atoi(inst[1:])
 			if err != nil {
@@ -30,109 +37,64 @@ func partOne(wires [][]string) {
 			}
 			if inst[0] == 'U' {
 				for i := y + 1; i <= y+num; i++ {
-					visited[point{x, i}] = true
+					steps += 1
+					setVisited(visited, point{x, i}, steps)
 				}
 				y += num
 			} else if inst[0] == 'D' {
 				for i := y - 1; i >= y-num; i-- {
-					visited[point{x, i}] = true
+					steps += 1
+					setVisited(visited, point{x, i}, steps)
 				}
 				y -= num
 			} else if inst[0] == 'L' {
 				for i := x - 1; i >= x-num; i-- {
-					visited[point{i, y}] = true
+					steps += 1
+					setVisited(visited, point{i, y}, steps)
 				}
 				x -= num
 			} else if inst[0] == 'R' {
 				for i := x + 1; i <= x+num; i++ {
-					visited[point{i, y}] = true
+					steps += 1
+					setVisited(visited, point{i, y}, steps)
 				}
 				x += num
 			}
 		}
-		for p := range visited {
-			pointMap[p] += 1
+
+		for p, vInfo := range visited {
+			pInfo := pointMap[p]
+			pInfo.steps += vInfo.steps
+			pInfo.visited += 1
+			pointMap[p] = pInfo
 		}
 	}
-
-	var min point
-	isSet := false
-	for p, value := range pointMap {
-		if value < 2 {
+	var minDist *point
+	var minStep *point
+	for p, pInfo := range pointMap {
+		if pInfo.visited < 2 {
 			continue
 		}
-		if !isSet || distance(min) > distance(p) {
-			min = p
-			isSet = true
+		pCopy := p
+		if minDist == nil || distance(*minDist) > distance(p) {
+			minDist = &pCopy
+		}
+		if minStep == nil || pointMap[*minStep].steps > pInfo.steps {
+			minStep = &pCopy
 		}
 	}
-	log.Print(min, distance(min))
+	log.Print("part one: ", distance(*minDist))
+	log.Print("part two: ", pointMap[*minStep].steps)
 	// print(pointMap)
 }
 
-func partTwo(wires [][]string) {
-	type info [2]uint
-	pointMap := make(map[point]info)
-
-	for _, wire := range wires {
-		visited := make(map[point]uint)
-		x, y := 0, 0
-		var step uint = 0
-
-		for _, inst := range wire {
-
-			num, _ := strconv.Atoi(inst[1:])
-			if inst[0] == 'U' {
-				for i := y + 1; i <= y+num; i++ {
-					step += 1
-					visited[point{x, i}] = step
-				}
-				y += num
-			} else if inst[0] == 'D' {
-				for i := y - 1; i >= y-num; i-- {
-					step += 1
-					visited[point{x, i}] = step
-				}
-				y -= num
-			} else if inst[0] == 'L' {
-				for i := x - 1; i >= x-num; i-- {
-					step += 1
-					visited[point{i, y}] = step
-				}
-				x -= num
-			} else if inst[0] == 'R' {
-				for i := x + 1; i <= x+num; i++ {
-					step += 1
-					visited[point{i, y}] = step
-				}
-				x += num
-			}
-
-		}
-		for p, step := range visited {
-			pInfo := pointMap[p]
-			if pInfo[0] != 0 {
-				pInfo[0] += 1
-				pInfo[1] += step
-				pointMap[p] = pInfo
-			} else {
-				pointMap[p] = info{1, step}
-			}
+func setVisited(visited map[point]*info, p point, steps uint) {
+	if visited[p] == nil {
+		visited[p] = &info{
+			visited: 1,
+			steps:   steps,
 		}
 	}
-
-	var min point
-	isSet := false
-	for p, value := range pointMap {
-		if value[0] < 2 {
-			continue
-		}
-		if !isSet || pointMap[min][1] > value[1] {
-			min = p
-			isSet = true
-		}
-	}
-	log.Print(min, pointMap[min][1])
 }
 
 func parseFile(path string) [][]string {
@@ -151,7 +113,7 @@ func parseFile(path string) [][]string {
 	return wires
 }
 
-func distance(p Point) int {
+func distance(p point) int {
 	return abs(p.x) + abs(p.y)
 }
 
@@ -159,26 +121,27 @@ func abs(n int) int {
 	return ((n ^ (n >> 31)) - (n >> 31))
 }
 
-func print(pointMap map[point]uint16) {
+func print(pointMap map[point]info) {
 	xMin, yMin, xMax, yMax := 0, 0, 0, 0
 	for p := range pointMap {
-		if yMax < p[1] {
-			yMax = p[1]
-		} else if yMin > p[1] {
-			yMin = p[1]
+		if yMax < p.y {
+			yMax = p.y
+		} else if yMin > p.y {
+			yMin = p.y
 		}
-		if xMax < p[0] {
-			xMax = p[0]
-		} else if xMin > p[0] {
-			xMin = p[0]
+		if xMax < p.x {
+			xMax = p.x
+		} else if xMin > p.x {
+			xMin = p.x
 		}
 	}
 
 	for y := yMax; y >= yMin; y-- {
 		for x := xMin; x <= xMax; x++ {
-			if pointMap[point{x, y}] == 1 {
+			pointInfo := pointMap[point{x, y}]
+			if pointInfo.visited == 1 {
 				fmt.Print("-")
-			} else if pointMap[point{x, y}] > 1 {
+			} else if pointInfo.visited > 1 {
 				fmt.Print("X")
 			} else if x == 0 && y == 0 {
 				fmt.Print("O")
