5c9f5b036cb8cf25b1a016c95a0b1a5475ab3b74
Viktor Seres <viktor.seres@ibc.lan>
2023-07-25 12:46:17 +0200

    add part two

--- day16/main.go
+++ day16/main.go
@@ -2,29 +2,50 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"io"
+	"math"
 	"os"
 	"strconv"
+	"strings"
 )
+
+var offset int
 
 func main() {
 	input := parseInput("input.txt")
-	pattern := []int{0, 1, 0, -1}
-	output := make([]int16, len(input))
+	partOne := make([]int16, len(input))
+	copy(partOne, input)
+	fmt.Println("Part one: ", FFT(partOne))
 
-	FFT(input, output, pattern)
+	for i := 0; i < 7; i++ {
+		offset += int(math.Pow10(6-i)) * int(input[i])
+	}
+	partTwo := make([]int16, len(input)*10000)
+	for i := 0; i < len(partTwo); i++ {
+		partTwo[i] = input[i%len(input)]
+	}
+	fmt.Println("part two: ", FFT2(partTwo[offset:]))
 }
 
-func FFT(input []int16, output []int16, patter []int) {
+func FFT(input []int16) string {
+	output := make([]int16, len(input))
 	for phase := 0; phase < 100; phase++ {
 		for i := range input {
-			output[i] = applyPatter(input, patter, i+1)
+			output[i] = applyPatter(input, i+1)
 		}
 		copy(input, output)
 	}
+	var buff strings.Builder
+	for i := 0; i < 8; i++ {
+		buff.WriteByte(byte(output[i]) + 48)
+	}
+	return buff.String()
 }
 
-func applyPatter(arr []int16, pattern []int, num int) int16 {
+var pattern = []int{0, 1, 0, -1}
+
+func applyPatter(arr []int16, num int) int16 {
 	sum := 0
 	repeat := num
 	mult := pattern[0]
@@ -37,7 +58,6 @@ func applyPatter(arr []int16, pattern []int, num int) int16 {
 			mult = pattern[index]
 			repeat = num
 		}
-		// fmt.Printf("%d*%d + ", mult, v)
 		sum += mult * int(v)
 		repeat--
 	}
@@ -45,8 +65,27 @@ func applyPatter(arr []int16, pattern []int, num int) int16 {
 	if sum < 0 {
 		sum = -sum
 	}
-	// fmt.Println(" = ", sum%10)
 	return int16(sum % 10)
+}
+
+func FFT2(arr []int16) string {
+	out := make([]int16, len(arr))
+	for phase := 0; phase < 100; phase++ {
+		sum := 0
+		for j := 0; j < len(arr); j++ {
+			sum += int(arr[j])
+		}
+		for i := 0; i < len(arr); i++ {
+			out[i] = int16((sum % 10))
+			sum -= int(arr[i])
+		}
+		copy(arr, out)
+	}
+	var buff strings.Builder
+	for i := 0; i < 8; i++ {
+		buff.WriteByte(byte(out[i]) + 48)
+	}
+	return buff.String()
 }
 
 func parseInput(name string) []int16 {
@@ -58,7 +97,10 @@ func parseInput(name string) []int16 {
 		if err == io.EOF {
 			break
 		}
-		num, _ := strconv.Atoi(string(char))
+		num, err := strconv.Atoi(string(char))
+		if err != nil {
+			fmt.Println(err, char)
+		}
 		input = append(input, int16(num))
 	}
 	return input
